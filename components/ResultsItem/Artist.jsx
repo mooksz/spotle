9c04cf3fa16b599c68e8d04482b66af6c8923757
Mike Haklander <mike@the-pack.nl>
2022-12-05 16:49:25 +0100

    Rename styles import resultsitems

--- components/ResultsItem/Artist.jsx
+++ components/ResultsItem/Artist.jsx
@@ -1,4 +1,4 @@
-import globalStyles from "./ResultsItem.module.scss";
+import resultsItemStyles from "./ResultsItem.module.scss";
 import Image from "next/image";
 import ExternalLinkWhiteIcon from "@/components/Icons/external-link-white.svg";
 import { Anchor } from "@/components/Anchor/Anchor";
@@ -8,33 +8,33 @@ export const Artist = (props) => {
 	const image = images.length ? images[0] : null;
 
 	return (
-		<div className={globalStyles["artist"]}>
+		<div className={resultsItemStyles["artist"]}>
 			<Anchor
 				href={external_urls?.spotify}
 				target='_blank'
 				wrapper
-				className={globalStyles["link"]}>
-				<div className={globalStyles["image-wrapper"]}>
+				className={resultsItemStyles["link"]}>
+				<div className={resultsItemStyles["image-wrapper"]}>
 					{image?.url && (
 						<Image
 							src={image.url}
 							width={image.width}
 							height={image.height}
 							alt={`Artist: ${name}`}
-							className={globalStyles["image"]}
+							className={resultsItemStyles["image"]}
 						/>
 					)}
 				</div>
 
-				<div className={globalStyles["info-wrapper"]}>
-					<div className={globalStyles["name"]}>{name}</div>
-					<div className={globalStyles["info"]}>
-						<span className={globalStyles["type"]}>{type} - </span>
-						<span className={globalStyles["genres"]}>{genres.join(" - ")}</span>
+				<div className={resultsItemStyles["info-wrapper"]}>
+					<div className={resultsItemStyles["name"]}>{name}</div>
+					<div className={resultsItemStyles["info"]}>
+						<span className={resultsItemStyles["type"]}>{type} - </span>
+						<span className={resultsItemStyles["genres"]}>{genres.join(" - ")}</span>
 					</div>
 				</div>
 
-				<ExternalLinkWhiteIcon className={globalStyles["link-icon"]} />
+				<ExternalLinkWhiteIcon className={resultsItemStyles["link-icon"]} />
 			</Anchor>
 		</div>
 	);
--- components/ResultsItem/Track.jsx
+++ components/ResultsItem/Track.jsx
@@ -1,4 +1,4 @@
-import globalStyles from "./ResultsItem.module.scss";
+import resultsItemStyles from "./ResultsItem.module.scss";
 import Image from "next/image";
 import ExternalLinkWhiteIcon from "@/components/Icons/external-link-white.svg";
 import { Anchor } from "@/components/Anchor/Anchor";
@@ -19,34 +19,34 @@ export const Track = (props) => {
 			: null;
 
 	return (
-		<div className={globalStyles["track"]}>
+		<div className={resultsItemStyles["track"]}>
 			<Anchor
 				href={external_urls?.spotify}
 				target='_blank'
 				wrapper
-				className={globalStyles["link"]}>
-				<div className={globalStyles["image-wrapper"]}>
+				className={resultsItemStyles["link"]}>
+				<div className={resultsItemStyles["image-wrapper"]}>
 					{image?.url && (
 						<Image
 							src={image.url}
 							width={image.width}
 							height={image.height}
 							alt={`Artist: ${name}`}
-							className={globalStyles["image"]}
+							className={resultsItemStyles["image"]}
 						/>
 					)}
 				</div>
 
-				<div className={globalStyles["info-wrapper"]}>
-					<div className={globalStyles["name"]}>{name}</div>
-					<div className={globalStyles["info"]}>
-						<span className={globalStyles["type"]}>{type}</span>
+				<div className={resultsItemStyles["info-wrapper"]}>
+					<div className={resultsItemStyles["name"]}>{name}</div>
+					<div className={resultsItemStyles["info"]}>
+						<span className={resultsItemStyles["type"]}>{type}</span>
 					</div>
 				</div>
 
 				{SaveButton}
 
-				<ExternalLinkWhiteIcon className={globalStyles["link-icon"]} />
+				<ExternalLinkWhiteIcon className={resultsItemStyles["link-icon"]} />
 			</Anchor>
 		</div>
 	);
